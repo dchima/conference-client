d85cab3010350badadb590ac877d6fd6c61ccf23
dchima <dcnnad@gmail.com>
2019-09-21 08:57:47 +0100

    ft: added talks view

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,19 @@ import React from 'react';
 
 const Header = () => {
   const styles = {
-    color: '#ef38dd',
+    color: '#ffffff',
     backgroundColor: 'blue',
     fontFamily: 'monospace',
+    fontSize: '80px',
+    textAlign: 'center',
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'center',
   };
 
   return (
-    <header className="navbar" style={styles}>
-      {'CONFERENCE API'}
+    <header style={styles}>
+      {'CONFERENCE CLIENT'}
     </header>
   );
 };
